4c227a6674f2c2b553298a052a42a3fa84f719fe
rachit18-arch <61114746+rachit18-arch@users.noreply.github.com>
2024-04-12 12:21:32 +0530

    draggable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1322,3 +1322,32 @@ function logout() {
     localStorage.clear();
     window.location.replace('login.html');
 }
+
+var draggable = document.getElementsByClassName('draggable')[0];
+if (draggable) {
+    var posX = 0,
+        posY = 0,
+        mouseX = 0,
+        mouseY = 0;
+
+    draggable.addEventListener('mousedown', mouseDown, false);
+    window.addEventListener('mouseup', mouseUp, false);
+
+    function mouseDown(e) {
+        e.preventDefault();
+        posX = e.clientX - draggable.offsetLeft;
+        posY = e.clientY - draggable.offsetTop;
+        window.addEventListener('mousemove', moveElement, false);
+    }
+
+    function mouseUp() {
+        window.removeEventListener('mousemove', moveElement, false);
+    }
+
+    function moveElement(e) {
+        mouseX = e.clientX - posX;
+        mouseY = e.clientY - posY;
+        document.getElementById('orderPanel').style.left = mouseX + 'px';
+        document.getElementById('orderPanel').style.top = mouseY + 'px';
+    }
+}
